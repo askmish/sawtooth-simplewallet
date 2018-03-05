d8c6b35927c4d0032032607d10a3b65a480512d2
ram-srini <ramakrishna.srinivasamurthy@intel.com>
2018-03-05 15:07:35 +0530

    Fixed the formatting and added proper comments

--- javaprocessor/src/main/java/simplewallet/processor/SimpleWalletProcessor.java
+++ javaprocessor/src/main/java/simplewallet/processor/SimpleWalletProcessor.java
@@ -27,9 +27,9 @@ public class SimpleWalletProcessor {
 	if (args.length != 1) {
 	    logger.info("Missing argument!! Please pass validator connection string");
 	}
-	// step 1. connect to validator with connection string (tcp://validator:4004)
+	// Connect to validator with connection string (tcp://validator:4004)
 	TransactionProcessor simpleWalletProcessor = new TransactionProcessor(args[0]);
-	// Create simple wallet transaction handler and register with validator
+	// Create simple wallet transaction handler and register with the validator
 	simpleWalletProcessor.addHandler(new SimpleWalletHandler());
 	Thread thread = new Thread(simpleWalletProcessor);
 	//start the transaction processor
@@ -41,10 +41,10 @@ public class SimpleWalletProcessor {
 /* ******************************************************************************
  * SimpleWalletHandler
  *
- * This class will be registered as the transaction processor handler
- * with validator
- * It sets the namespaceprefix, versions and types of operations
- * that can be handled by this TP
+ * It handles the processing of operation supported by simplewallet.
+ * It sets the name space prefix, versions and transaction family name.
+ * This is the place where you implement your transaction specific logic(Insie apply() method
+ * 
  ***************************************************************************** */
 
 class SimpleWalletHandler implements TransactionHandler {
@@ -55,125 +55,126 @@ class SimpleWalletHandler implements TransactionHandler {
     private String simpleWalletNameSpace;
 
     SimpleWalletHandler() {
-        try {
-		//Initialize the simple wallet namespace using first 6 characters
-                simpleWalletNameSpace = Utils.hash512(txnFamilyName.getBytes("UTF-8")).substring(0, 6);
-        } catch (java.io.UnsupportedEncodingException ex) {
-		System.out.println("Unsupported the encoding format ");
-		ex.printStackTrace();
-		System.exit(1);
-        }
+	try {
+	    //Initialize the simple wallet name space using first 6 characters
+	    simpleWalletNameSpace = Utils.hash512(txnFamilyName.getBytes("UTF-8")).substring(0, 6);
+	} catch (java.io.UnsupportedEncodingException ex) {
+	    System.out.println("Unsupported the encoding format ");
+	    ex.printStackTrace();
+	    System.exit(1);
+	}
     }
 
     @Override
     public void apply(TpProcessRequest request, State stateInfo) throws InvalidTransactionException, InternalError {
-	/* This method is invoked by validater to 
+	/* This method is invoked by validator to 
 	   perform actual business logic and do ledger state manipulation
 	   Here we calling to operations like deposit and withdraw 
-	*/
-        
+	 */
+
 	/* Get operation request from payload data, we are passing operation name(deposit, withdraw)
 	 * and amount in the payload from the client. */
 
-        String payload =  request.getPayload().toStringUtf8();
-        ArrayList<String> payloadList = new ArrayList<>(Arrays.asList(payload.split(",")));
-        if(payloadList.size() != 2) {
-            throw new InvalidTransactionException("Invalid no. of arguments: expected 2, got:" + payloadList.size());
-        }
-        // First argument from payload is operation name
-        String operation = payloadList.get(0);
+	String payload =  request.getPayload().toStringUtf8();
+	ArrayList<String> payloadList = new ArrayList<>(Arrays.asList(payload.split(",")));
+	if(payloadList.size() != 2) {
+	    throw new InvalidTransactionException("Invalid no. of arguments: expected 2, got:" + payloadList.size());
+	}
+	// First argument from payload is operation name
+	String operation = payloadList.get(0);
 	// Get the amount
-        Integer amount = Integer.valueOf(payloadList.get(1));
-        // Get the user signing public key from header
-        String userKey = request.getHeader().getSignerPublicKey();
-        switch(operation) {
-        case "deposit" :
-            makeDeposit(stateInfo, operation, amount, userKey);
-            break;
-        case "withdraw":
-            makeWithdraw(stateInfo, operation, amount, userKey);
-            break;
-	/* Add here you custom operation to perform and make changes in client as well
-	 * pass the corresponding operation name in the payload */
+	Integer amount = Integer.valueOf(payloadList.get(1));
+	// Get the user signing public key from header
+	String userKey = request.getHeader().getSignerPublicKey();
+	switch(operation) {
+	case "deposit" :
+	    makeDeposit(stateInfo, operation, amount, userKey);
+	    break;
+	case "withdraw":
+	    makeWithdraw(stateInfo, operation, amount, userKey);
+	    break;
+	    /* Add here you custom operation to perform and make changes in client as well
+	     * pass the corresponding operation name in the payload.
+	     * */
 
-        default:
-            String error = "Unsupported operation " + operation;
-            throw new InvalidTransactionException(error);
-        }
+	default:
+	    String error = "Unsupported operation " + operation;
+	    throw new InvalidTransactionException(error);
+	}
     }
 
     @Override
     public Collection<String> getNameSpaces() {
-        ArrayList<String> namespaces = new ArrayList<>();
-        namespaces.add(simpleWalletNameSpace);
-        return namespaces;
+	ArrayList<String> namespaces = new ArrayList<>();
+	namespaces.add(simpleWalletNameSpace);
+	return namespaces;
     }
 
     @Override
     public String getVersion() {
-        return version;
+	return version;
     }
 
     @Override
     public String transactionFamilyName() {
-        return txnFamilyName;
+	return txnFamilyName;
     }
 
     private void makeDeposit(State stateInfo, String operation, Integer amount, String userKey)
-	   throws InvalidTransactionException, InternalError {
-        //Get the wallet key from the signer public key
-        String walletKey = getWalletKey(userKey);
-        logger.info("Got user key " + userKey + "wallet key " + walletKey);
-        //Get balance from ledger state
+	    throws InvalidTransactionException, InternalError {
+	//Get the wallet key from the signer public key
+	String walletKey = getWalletKey(userKey);
+	logger.info("Got user key " + userKey + "wallet key " + walletKey);
+	//Get balance from ledger state
 	Map<String, ByteString> currentLedgerEntry = stateInfo.getState(Collections.singletonList(walletKey));
-        String balance = currentLedgerEntry.get(walletKey).toStringUtf8();
-        Integer newBalance = 0;
+	String balance = currentLedgerEntry.get(walletKey).toStringUtf8();
+	Integer newBalance = 0;
 	// getState() will return empty map if won't able to find the walletkey in state store
-        if (balance.isEmpty()) {
-            logger.info("This is the first time we got a deposit for user.");
-            logger.info("Creating a new account for the user: " + userKey);
-            newBalance = amount;
-        }
-        else {
-            newBalance = Integer.valueOf(balance) + amount;
-        }
-        // Update balance in the ledger state
-        Map.Entry<String, ByteString> entry = new AbstractMap.SimpleEntry<String, ByteString>(walletKey,
-                ByteString.copyFromUtf8(newBalance.toString()));
-        Collection<Map.Entry<String, ByteString>> newLedgerEntry = Collections.singletonList(entry);
-        logger.info("Crediting balance with " + amount);
-        stateInfo.setState(newLedgerEntry);
+	if (balance.isEmpty()) {
+	    logger.info("This is the first time we got a deposit for user.");
+	    logger.info("Creating a new account for the user: " + userKey);
+	    newBalance = amount;
+	}
+	else {
+	    newBalance = Integer.valueOf(balance) + amount;
+	}
+	// Update balance in the ledger state
+	Map.Entry<String, ByteString> entry = new AbstractMap.SimpleEntry<String, ByteString>(walletKey,
+		ByteString.copyFromUtf8(newBalance.toString()));
+	Collection<Map.Entry<String, ByteString>> newLedgerEntry = Collections.singletonList(entry);
+	logger.info("Crediting balance with " + amount);
+	stateInfo.setState(newLedgerEntry);
     }
-    
+
     private void makeWithdraw(State stateInfo, String operation, Integer amount, String userKey)
 	    throws InvalidTransactionException, InternalError {
-        String walletKey = getWalletKey(userKey);
-        logger.info("Got user key " + userKey + "wallet key "+ walletKey);
-        //Get balance from ledger state
+	String walletKey = getWalletKey(userKey);
+	logger.info("Got user key " + userKey + "wallet key "+ walletKey);
+	//Get balance from ledger state
 	Map<String, ByteString> currentLedgerEntry = stateInfo.getState(Collections.singletonList(walletKey));
-        String balance = currentLedgerEntry.get(walletKey).toStringUtf8();
-        if (balance.isEmpty()) {
-            String error = "Didn't find the wallet key associated with user key " + userKey;
+	String balance = currentLedgerEntry.get(walletKey).toStringUtf8();
+	if (balance.isEmpty()) {
+	    String error = "Didn't find the wallet key associated with user key " + userKey;
 	    throw new InvalidTransactionException(error);
-        }
-        Integer value = Integer.valueOf(balance);
-        if (value < amount) {
-            String error = "Withdraw amount should be lesser than or equal to " + value;
-            throw new InvalidTransactionException(error);
-        }
-        // Update balance
-        Integer updateBalance = value - amount;
-        Map.Entry<String, ByteString> entry = new AbstractMap.SimpleEntry<String, ByteString>(walletKey,
-		       ByteString.copyFromUtf8(updateBalance.toString()));
-        Collection<Map.Entry<String, ByteString>> newLedgerEntry = Collections.singletonList(entry);
-        logger.info("Debitting balance with " + amount);
-        stateInfo.setState(newLedgerEntry);
-     }
-    
+	}
+	Integer value = Integer.valueOf(balance);
+	if (value < amount) {
+	    String error = "Withdraw amount should be lesser than or equal to " + value;
+	    throw new InvalidTransactionException(error);
+	}
+	// Update balance
+	Integer updateBalance = value - amount;
+	Map.Entry<String, ByteString> entry = new AbstractMap.SimpleEntry<String, ByteString>(walletKey,
+		ByteString.copyFromUtf8(updateBalance.toString()));
+	Collection<Map.Entry<String, ByteString>> newLedgerEntry = Collections.singletonList(entry);
+	logger.info("Debitting balance with " + amount);
+	stateInfo.setState(newLedgerEntry);
+    }
+
     private String getWalletKey(String userKey) {
-        //Generate unique key(wallet key) from the wallet namespace and user signer key
-        return Utils.hash512(txnFamilyName.getBytes()).substring(0, 6)
-                + Utils.hash512(userKey.getBytes()).substring(0, 64);
+	//Generate unique key(wallet key) from the wallet namespace and user signer key
+	return Utils.hash512(txnFamilyName.getBytes()).substring(0, 6)
+		+ Utils.hash512(userKey.getBytes()).substring(0, 64);
     }
 }
 
