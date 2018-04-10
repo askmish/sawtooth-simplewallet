4507610fbee9b04d8516d3c484090ea40cc367fc
Ashish Kumar Mishra <ashish.k.mishra@intel.com>
2018-04-11 00:11:59 +0530

    Add transfer operation for cpp tp
    Signed-off-by: Ashish Kumar Mishra <ashish.k.mishra@intel.com>

--- cxxprocessor/simplewallet-tp.cpp
+++ cxxprocessor/simplewallet-tp.cpp
@@ -69,15 +69,25 @@ std::vector<std::string> split(const std::string& str, char delimiter)
 }
 
 // Helper function: To extract Action str and value integer from given string
-void strToActionAndValue(const std::string& str, std::string& action, uint32_t& value) {
+// and beneficiary string if available
+void strToActionValueAndBeneficiary(const std::string& str,
+                                    std::string& action,
+                                    uint32_t& value,
+                                    std::string& beneficiary) {
      std::vector<std::string> vs = split(str, ',');
-     if (vs.size() != 2) {
-         std::string error = "invalid no. of arguments: expected 2, got:"
+
+     if (vs.size() == 2) {
+         action = vs[0];
+         value = std::stoi(vs[1]);
+     } else if (vs.size() == 3) {
+         action = vs[0];
+         value = std::stoi(vs[1]);
+         beneficiary = vs[2];
+     } else {
+         std::string error = "invalid no. of arguments: expected 2 or 3, got:"
              + std::to_string(vs.size()) + "\n";
          throw sawtooth::InvalidTransaction(error);
      }
-     action = vs[0];
-     value = std::stoi(vs[1]);
 }
 
 /*******************************************************************************
@@ -103,17 +113,24 @@ class SimpleWalletApplicator:  public sawtooth::TransactionApplicator {
 
         std::string action;
         uint32_t value;
+        std::string beneficiary_pubkey;
 
         // Extract the action and value from the payload string
-        strToActionAndValue(raw_data, action, value);
-        std::cout << "Got: " << action << " and " << value << "\n";
+        strToActionValueAndBeneficiary(raw_data,
+                                       action,
+                                       value,
+                                       beneficiary_pubkey);
 
+        std::cout << "Got: " << action << " and " << value << "\n";
 
         // Choose what to do with value, based on action
         if (action == "deposit") {
             this->makeDeposit(wallet_user_pubkey, value);
         } else if (action == "withdraw") {
             this->doWithdraw(wallet_user_pubkey, value);
+        } else if (action == "transfer") {
+            std::cout << "Got beneficiary: " << beneficiary_pubkey << "\n";
+            this->doTransfer(wallet_user_pubkey, value, beneficiary_pubkey);
         }
         // Add your own action and a corresponding handler here
         // Also add the actions in the client app as well
@@ -201,6 +218,72 @@ class SimpleWalletApplicator:  public sawtooth::TransactionApplicator {
         LOG4CXX_DEBUG(logger, "Storing " << stored_value << " units");
         stored_value_str = std::to_string(stored_value);
         this->state->SetState(address, stored_value_str);
+    }
+
+    // Handle SimpleWallet Transfer action.
+    void doTransfer(const std::string& customer_pubkey,
+                    const uint32_t& request_amount,
+                    const std::string& beneficiary_pubkey) {
+
+        // Get the global state address for each user's account
+        // based on respective pubkeys
+        auto customer_state_address = this->MakeAddress(customer_pubkey);
+        auto beneficiary_state_address = this->MakeAddress(beneficiary_pubkey);
+
+        LOG4CXX_DEBUG(logger, "SimpleWalletApplicator::doTransfer Key: "
+            << customer_pubkey
+            << " Address: " << customer_state_address);
+
+        LOG4CXX_DEBUG(logger, "SimpleWalletApplicator::doTransfer Beneficiary "
+            << "Key: "
+            << beneficiary_pubkey
+            << " Address: " << beneficiary_state_address);
+
+        std::string stored_balance_str;
+
+        // Retrieve the balance available for customer account
+        uint32_t customer_available_balance = 0;
+        if(this->state->GetState(&stored_balance_str, customer_state_address)) {
+             customer_available_balance = std::stoi(stored_balance_str);
+        } else {
+            // The customer account hasn't been created yet
+            std::string error = "Action was 'transfer', but address"
+                " not found in state for customer Key: " + customer_pubkey;
+            throw sawtooth::InvalidTransaction(error);
+        }
+
+        // Retrieve the balance available for beneficiary account 
+        uint32_t beneficiary_available_balance = 0;
+        if(this->state->GetState(&stored_balance_str, beneficiary_state_address)) {
+             beneficiary_available_balance = std::stoi(stored_balance_str);
+        } else {
+            // The beneficiary account hasn't been created yet
+            std::string error = "Action was 'transfer', but address"
+                " not found in state for beneficiary Key: " + beneficiary_pubkey;
+            throw sawtooth::InvalidTransaction(error);
+        }
+
+        // Verify if customer has sufficient balance to transfer requested amount
+        if (customer_available_balance < request_amount) {
+            std::string error = "Insufficient balance. Can't transfer from customer: "
+                                + customer_pubkey;
+            throw sawtooth::InvalidTransaction(error);
+        }
+
+        // Update the balance for customer and store in global state
+        customer_available_balance -= request_amount;
+        LOG4CXX_DEBUG(logger, "Finalizing " << customer_available_balance
+                               << " units for customer: " << customer_pubkey);
+        stored_balance_str = std::to_string(customer_available_balance);
+        this->state->SetState(customer_state_address, stored_balance_str);
+
+        // Update the balance for beneficiary and store in global state
+        beneficiary_available_balance += request_amount;
+        LOG4CXX_DEBUG(logger, "Finalizing " << beneficiary_available_balance
+                               << " units for beneficiary: "
+                               << beneficiary_pubkey);
+        stored_balance_str = std::to_string(beneficiary_available_balance);
+        this->state->SetState(beneficiary_state_address, stored_balance_str);
     }
 };
 
